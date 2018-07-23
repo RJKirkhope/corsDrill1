1c71e90c32f984e9f849e6713b8245f1900f1582
Rjkirkhope <rjkirkhope@gmail.com>
2018-07-23 13:20:10 -0600

    Added Heroku

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 9000
+const data = require('./api/cohorts')
+
+app.use(cors())
+
+function dataId(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i]
+    }
+  }
+}
+
+app.get('/', function(request, response){
+  response.json({
+    data
+  })
+})
+
+app.get('/:id', function(request, response){
+  var cohort = dataId(data, request.params.id)
+  if (!cohort) {
+    response.status(404).json({
+      error: {
+        message: 'No cohort found!'
+      }
+    })
+  }else {
+    response.json({
+      data: cohort
+    })
+  }
+})
+
+app.listen(port, () => console.log('listening on http://localhost:9000'))
